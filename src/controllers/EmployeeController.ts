f566ae284a56836ee350f9f5e99b956424086c82
sataporn1995 <satapornchai2538@gmail.com>
2022-11-28 10:39:20 +0700

    Update

--- src/controllers/EmployeeController.ts
+++ src/controllers/EmployeeController.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
+import EmployeeModel from "../models/EmployeeModel";
 
 export default class EmployeeController {
-  public static getEmployees(req: Request, res: Response) {
-    res.send("Hello World!");
+  public static async getEmployees(req: Request, res: Response) {
+    try {
+      const data = await EmployeeModel.getEmployees();
+      return res.send(data);
+    } catch (error) {
+      return res.send([]);
+    }
   }
 }
--- src/models/EmployeeModel.ts
+++ src/models/EmployeeModel.ts
@@ -3,41 +3,41 @@ import dbconfig from "../config/dbconfig";
 import { EmployeeInterface } from "./interfaces";
 
 export default class EmployeeModel {
-  private sql: SQL;
+  private static sql: SQL;
 
   public constructor() {
-    this.sql = new SQL(dbconfig);
+    EmployeeModel.sql = new SQL(dbconfig);
   }
 
   //! C = Create
-  public async createEmployee(Name: string, StartDate: string) {
+  public static async createEmployee(Name: string, StartDate: string) {
     const query = `INSERT INTO Employees (Name, StartDate) VALUES ('${Name}', '${StartDate}');`;
-    const result = await this.sql.execute(query);
+    const result = await EmployeeModel.sql.execute(query);
     return { data: result.rowsAffected, error: null };
   }
 
   //! R = Read
-  public async getEmployees(): Promise<EmployeeInterface[]> {
+  public static async getEmployees(): Promise<EmployeeInterface[]> {
     const query = "SELECT EmployeeID,Name,StartDate FROM Employees;";
-    const result = await this.sql.execute(query);
+    const result = await EmployeeModel.sql.execute(query);
     return result.recordset;
   }
 
   //! U = Update
-  public async updateEmployee(
+  public static async updateEmployee(
     EmployeeID: number,
     Name: string,
     StartDate: string
   ) {
     const query = `UPDATE Employees SET Name='${Name}', StartDate='${StartDate}' WHERE EmployeeID=${EmployeeID};`;
-    const result = await this.sql.execute(query);
+    const result = await EmployeeModel.sql.execute(query);
     return result.rowsAffected;
   }
 
   //! D = Delete
-  public async deleteEmployee(EmployeeID: number) {
+  public static async deleteEmployee(EmployeeID: number) {
     const query = `DELETE FROM Employees WHERE EmployeeID=${EmployeeID};`;
-    const result = await this.sql.execute(query);
+    const result = await EmployeeModel.sql.execute(query);
     return result.rowsAffected;
   }
 }
