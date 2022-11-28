abb19a5b8f915539431457aba83818e318fdacbd
sataporn1995 <satapornchai2538@gmail.com>
2022-11-28 11:22:54 +0700

    Update

--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,8 @@ import EmployeeController from "./controllers/EmployeeController";
 const router = Router();
 
 router.get("/employees", EmployeeController.getEmployees);
+router.post("/add-employee", EmployeeController.createEmployee);
+router.post("/update-employee", EmployeeController.updateEmployee);
+router.post("/delete-employee", EmployeeController.deleteEmployee);
 
 export default router;
